8121a5b0983cd4920deb47c033f15ae83a7453f5
camarello <camarello@student.dwc.edu>
2015-03-27 14:26:47 -0400

    [Declared All Graphics]
    Declared all the graphics at top. Horizontal and Vertical Ships

--- battleships 1st try/battleships 1st try/MyForm.h
+++ battleships 1st try/battleships 1st try/MyForm.h
@@ -1,14 +1,14 @@
 #pragma once
-<<<<<<< HEAD
+//<<<<<<< HEAD
 #include <cstdlib>
 #include <ctime>
 #include "player.h"
-=======
+//=======
 #include <stdlib.h>
 #include "player.h"
 #include "5cellship.h"
 
->>>>>>> origin/master
+//>>>>>>> origin/master
 namespace Project1 {
 
 	using namespace System;
@@ -59,7 +59,32 @@ namespace Project1 {
 	/////GRAPHICS//////////////	 
 		Graphics^ g;
 		Pen^ blackBrush;
-		Bitmap^ Ship_5 = gcnew Bitmap("Graphics/(250x50)_Colony_CarrierShip_[5Cells].png");
+		Bitmap^ Ship_5 = gcnew Bitmap("Graphics\(250x50)_Colony_CarrierShip_[5Cells].png");
+
+		/////////////////////////////////////////////////////////////////
+		///////////////////// Put Graphics Here /////////////////////////
+		Bitmap^ starfieldempty_png = gcnew Bitmap("Graphics\480x480)_Starfield[Empty].png"); //Empty Grid Picture
+		Bitmap^ starfieldgrid_png = gcnew Bitmap("Graphics\(480x480)_Starfield[Grid].png"); //Picture with Grid
+		// Logo
+		Bitmap^ logo_png = gcnew Bitmap("Graphics\(300x150)BattleStarShips_Logo.png"); //Game Logo
+		// Frigate PNG
+		Bitmap^ Ship_1_verticalpng = gcnew Bitmap("Graphics\(50x50)_Colony_FrigateShip_[1Cell].png"); //1 Cell Frigate Ship Facing Vertically
+		Bitmap^ Ship_1_horizontalpng = gcnew Bitmap("Graphics\(50x50)_Colony_FrigateShip_[1Cell]_RightFace.png"); //1 Cell Frigate Ship Facing Horizontally
+		// Stealth PNG
+		Bitmap^ Ship_2_verticalpng = gcnew Bitmap("Graphics\(50x100)_Colony_StealthShip_[2Cells].png"); //2 Cell Stealth Ship Facing Vertically
+		Bitmap^ Ship_2_horizontalpng = gcnew Bitmap("Graphics\(50x100)_Colony_StealthShip_[2Cells]_RightFace.png"); //2 Cell Stealth Ship Facing Horizontally
+		// Cruiser PNG
+		Bitmap^ Ship_3_verticalpng = gcnew Bitmap("Graphics\(50x150)_Colony_CruiserShip_[3Cells].png"); //3 Cell Cruiser Ship Facing Vertically
+		Bitmap^ Ship_3_horizontalpng = gcnew Bitmap("Graphics\(50x100)_(150x50)_Colony_CruiserShip_[3Cells]_RightFace.png"); //3 Cell Cruiser Ship Facing Horizontally
+		// Battleship PNG
+		Bitmap^ Ship_4_verticalpng = gcnew Bitmap("Graphics\(50x200)_Colony_BattleShip_[4Cells].png"); //4 Cell Battleship Ship Facing Vertically
+		Bitmap^ Ship_4_horizontalpng = gcnew Bitmap("Graphics\(200x50)_Colony_BattleShip_[4Cells]_[RightFace].png"); //4 Cell Battleship Ship Facing Horizontally
+		// Carrier PNG
+		Bitmap^ Ship_5_verticalpng = gcnew Bitmap("Graphics\(50x250)_Colony_CarrierShip_[5Cells].png"); //5 Cell Carrier Ship Facing Vertically
+		Bitmap^ Ship_5_horizontalpng = gcnew Bitmap("Graphics\(250x50)_Colony_CarrierShip_[5Cells]_RightFace.png"); //5 Cell Carrier Ship Facing Horizontally
+		////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////////////////
+
 
 	/////////////////////
 
@@ -157,8 +182,8 @@ namespace Project1 {
 	
 		int numrow = 40;
 		int numcol = 40;
-	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
-<<<<<<< HEAD
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) 
+	{
 				 g = pictureBox1->CreateGraphics();
 				 Box1 = gcnew array<player^, 2>(40, 40);
 				 for (int row = 0; row < numrow; row++)
@@ -174,9 +199,6 @@ namespace Project1 {
 
 					 }
 				 }
-	
-	
-=======
 
 				 g = pictureBox1->CreateGraphics();
 				 blackBrush = gcnew System::Drawing::Pen(Color::Black);
@@ -196,9 +218,9 @@ namespace Project1 {
 				// draw_grid();
 				 
 	}
-	private: System::Void textBox2_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	private: System::Void textBox2_TextChanged(System::Object^  sender, System::EventArgs^  e) 
+	{
 	}
-
 
 private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 
@@ -215,7 +237,6 @@ private: System::Void button1_Click(System::Object^  sender, System::EventArgs^ 
 					 g->DrawRectangle(blackBrush, lrggridsrect);
 				 }
 			 }
->>>>>>> origin/master
 }
 };
 }
@@ -227,7 +248,6 @@ System::Void Computer_move()
 	int x = rand() % 40 + 0;
 	int y = rand() % 40 + 0;
 	Box1[x, y]->status = 'r';
-
 
 /*
 Void draw_grid(){
@@ -246,8 +266,6 @@ Void draw_grid(){
 
 
 }
-
-
 
 //notes
 /*
--- battleships 1st try/battleships 1st try/player.h
+++ battleships 1st try/battleships 1st try/player.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once;
 using namespace System::Drawing;
 ref class player
@@ -11,7 +10,6 @@ public:
 	char status;
 
 };
-=======
 #pragma once
 
 ref class player{
@@ -33,11 +31,4 @@ public:
 	bool get_ship();
 	void set_ship(bool);
 
-
-
 };
-<<<<<<< HEAD
-=======
-*/
->>>>>>> origin/master
->>>>>>> origin/master
